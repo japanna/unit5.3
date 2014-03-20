2a3f03abe6a5dfbc23fa40b78e18bc92ab3923ee
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-03-20 15:21:32 -0500

    Final CaesarCipher without javadoc comments

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -48,7 +48,10 @@ public class CaesarCipher
 				if ((mode != 1) && (mode != 2) && (mode != -1)) throw new BadModeException();
 
 				// if user chose to exit
-				if (mode == -1) retry = false;
+				if (mode == -1) {
+					retry = false;
+					return;
+				}
 
 				System.out.print("\nWhat shift should I use? ");
 				// get the shift
@@ -57,10 +60,12 @@ public class CaesarCipher
 				if (shift < 0) throw new BadShiftException();
 				// if shift is larger than 26, "wrap around" by modulus to a number <= 26
 				if (shift > 26) shift %= 26;
-
+				
 				// get the name of the input file
 				System.out.print("\nWhat is the name of the input file? ");
 				inputFile = keyboard.next();
+				File f = new File(inputFile);
+				if(!f.exists()) throw new FileNotFoundException();
 
 				// set the name of the output file
 				System.out.print("\nWhat is the name of the output file? ");
@@ -69,28 +74,117 @@ public class CaesarCipher
 
 				// if user chose to encifer
 				if (mode == 1) {
-					encodedOutput = caesarEncipher (inputFile, shift);
-					// create PrintWriter object
-					PrintWriter outputWriter = new PrintWriter (outputFile);
-					// write encoded lines to outputFile
-
+					String encodedOutput = caesarEncipher(inputFile, shift);
+					// create PrintStream object
+					PrintStream outputStream = new PrintStream (new File(outputFile));
+					// write encoded message to outputFile
+					outputStream.print(encodedOutput);
+					System.out.print("\nDone!!!\n\n");
 				}
 				else {
-					//caesarDecipher (inputFile, shift);
-					// write to output file
+					String decodedOutput = caesarDecipher(inputFile, shift);
+					// create PrintStream object
+					PrintStream outputStream = new PrintStream (new File(outputFile));
+					// write encoded message to outputFile
+					outputStream.print(decodedOutput);
+					System.out.print("\nDone!!!\n\n");
 				}
 			}
-			catch(BadModeException e) {}
-			catch(BadShiftException e) {}
-			catch(FileNotFoundException e) {}
+			catch(BadModeException e) { 
+				System.out.print("\nSorry, I can't allow that. Enter 1 to encipher, 2 to decipher or -1 to exit:  ");
+			}
+			catch(InputMismatchException e) {
+				System.out.print("\nSorry. You have to type an integer. Exiting...\n\n");
+				keyboard.nextInt();
+			}
+			catch(BadShiftException e) {
+				System.out.print("\nSorry. Shift has to be larger than zero. Please start over.\n\n");
+			}
+			catch(FileNotFoundException e) {
+				System.out.println("\nSorry. That file can not be read or written to. Please start over. \n");
+			}
 
 		}
 
-		public static String caesarEncipher (String input, int shift) {
+	public static String caesarEncipher (String input, int shift) throws FileNotFoundException {
+			// create Scanner object
+			Scanner inFile = new Scanner(new File(input));
+		
+			// create StringBuilder object to hold enccoded lines of text
+			StringBuilder encodedString = new StringBuilder();
 
+			// while file has nextLine
+			while (inFile.hasNextLine()) {
+				// line to be encoded
+				String line = inFile.nextLine();
 
+				// get length of line
+				int length = line.length(); 
+					
+				// for each character in line
+				for (int i = 0; i < length; i++) {
+					// check if uppercase
+					if (Character.isUpperCase(line.charAt(i))) {
+							
+						// convert ASCII value of char to a value between 0-25 to make wraparound work
+						int rot25 = line.charAt(i) - 65;
+							
+						// calculate the cipher character
+						int cipher_char = (rot25 + shift) % 26;
 
+						// converts to ASCII value again and place in StringBuilder
+						encodedString.append((char)(cipher_char + 65));
+					}
+					// if not uppercase just add to StringBuilder object "as is"
+					else {
+						encodedString.append((char)line.charAt(i));
+					}			
+				}
+				//add a newline
+				encodedString.append("\n");
+	        }
+	        // return string with encoded message
+	        return encodedString.toString();		
+	}
 
-		}
+	public static String caesarDecipher (String input, int shift) throws FileNotFoundException {
+			// create Scanner object
+			Scanner inFile = new Scanner(new File(input));
+		
+			// create StringBuilder object to hold deccoded lines of text
+			StringBuilder decodedString = new StringBuilder();
 
+			// while file has nextLine
+			while (inFile.hasNextLine()) {
+				// line to be encoded
+				String line = inFile.nextLine();
+
+				// get length of line
+				int length = line.length(); 
+					
+				// for each character in line
+				for (int i = 0; i < length; i++) {
+					// check if uppercase
+					if (Character.isUpperCase(line.charAt(i))) {
+							
+						// convert ASCII value of char to a value between 0-25 to make wraparound work
+						int rot25 = line.charAt(i) - 65;
+							
+						// calculate the plaintext character
+						int decipher_char = (rot25 - shift) % 26;
+
+						// converts to ASCII value again and place in StringBuilder
+						decodedString.append((char)(decipher_char + 65));
+					}
+					// if not uppercase just add to StringBuilder object "as is"
+					else {
+						decodedString.append((char)line.charAt(i));
+					}			
+				}
+				//add a newline
+				decodedString.append("\n");
+	        }
+	        // return string with decoded message
+	        return decodedString.toString();		
+	}
 }
