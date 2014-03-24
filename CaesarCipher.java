e94271d6962d6b61b25b133ccddac429b1c4e3a8
Anna McKelvey <anna.e.mckelvey@gmail.com>
2014-03-24 15:47:14 -0500

    Final CaesarCipher.java, with javadoc comments

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -2,19 +2,18 @@
 
 /****************************************************************
  * 
- *  Encrypts a string through Caesar cipher
+ *  Encrypts or decrypts a string through Caesar cipher
  *	Only encrypts uppercase, alphabetical characters, 
  *	leaving other characters/numbers intact.
  *
  *  @author:  Anna Ntenta, anna.ntenta@gmail.com
- *  @version: Last Modified ___, 2014
+ *  @version: Last Modified 3/24, 2014
  *
  ****************************************************************/
 
 import java.util.*;
 import java.io.*;
 
-class BadModeException extends Exception {}
 class BadShiftException extends Exception {}
 
 public class CaesarCipher
@@ -36,15 +35,18 @@ public class CaesarCipher
 
 		boolean retry = true; 
 		// loop keeps asking until user provides correct input
-		while (retry) 
+		while (retry) {
 			try {
-				
 				System.out.print("Enter 1 to encipher, or 2 to decipher (-1 to exit): ");
+				// ask for the right input until given
+				while (!keyboard.hasNextInt()) {
+					keyboard.next();
+					System.out.println("\nYou have to type 1, 2 or -1. Please try again.\n");
+					System.out.print("Enter 1 to encipher, or 2 to decipher (-1 to exit): ");
+				}
 				// get the mode
 				mode = keyboard.nextInt();
-				// check that user provided correct input
-				if ((mode != 1) && (mode != 2) && (mode != -1)) throw new BadModeException();
-
+				
 				// if user chose to exit
 				if (mode == -1) {
 					retry = false;
@@ -52,6 +54,11 @@ public class CaesarCipher
 				}
 
 				System.out.print("\nWhat shift should I use? ");
+				while (!keyboard.hasNextInt()) {
+					keyboard.next();
+					System.out.println("\nSorry. You have to type an integer. Try again.");
+					System.out.print("\nWhat shift should I use? ");
+				}
 				// get the shift
 				shift = keyboard.nextInt();
 				// check that user provided correct input
@@ -78,6 +85,7 @@ public class CaesarCipher
 					// write encoded message to outputFile
 					outputStream.print(encodedOutput);
 					System.out.print("\nDone!!!\n\n");
+					outputStream.close();
 				}
 				else {
 					String decodedOutput = caesarDecipher(inputFile, shift);
@@ -86,10 +94,8 @@ public class CaesarCipher
 					// write encoded message to outputFile
 					outputStream.print(decodedOutput);
 					System.out.print("\nDone!!!\n\n");
+					outputStream.close();
 				}
-			}
-			catch(BadModeException e) { 
-				System.out.print("\nSorry, I can't allow that. Enter 1 to encipher, 2 to decipher or -1 to exit:  ");
 			}
 			catch(InputMismatchException e) {
 				System.out.print("\nSorry. You have to type an integer. Exiting...\n\n");
@@ -101,88 +107,118 @@ public class CaesarCipher
 			catch(FileNotFoundException e) {
 				System.out.println("\nSorry. That file can not be read or written to. Please start over. \n");
 			}
-
+		}
+		keyboard.close();
 		}
 
+	/**
+	 * Takes the name of an input file and an integer representing 
+	 * the amount of "shift" by which the contents of the file should
+	 * be encrypted and returns an encrypted string.
+	 *
+	 * @param   String inputFile
+	 * @param   int Shift
+	 * @throws: FileNotFoundException If input file can't be read
+	 */	
 	public static String caesarEncipher (String input, int shift) throws FileNotFoundException {
+		Scanner inFile = null;
+		try {
 			// create Scanner object
-			Scanner inFile = new Scanner(new File(input));
-		
-			// create StringBuilder object to hold enccoded lines of text
-			StringBuilder encodedString = new StringBuilder();
+			inFile = new Scanner(new File(input));
+		}
+		catch(FileNotFoundException e) {
+				System.out.println("\nSomething went wrong. That file can not be read or written to. Exiting... \n");
+		}
+		// create StringBuilder object to hold enccoded lines of text
+		StringBuilder encodedString = new StringBuilder();
 
-			// while file has nextLine
-			while (inFile.hasNextLine()) {
+		// while file has nextLine
+		while (inFile.hasNextLine()) {
 				// line to be encoded
-				String line = inFile.nextLine();
+			String line = inFile.nextLine();
 
-				// get length of line
-				int length = line.length(); 
+			// get length of line
+			int length = line.length(); 
 					
-				// for each character in line
-				for (int i = 0; i < length; i++) {
-					// check if uppercase
-					if (Character.isUpperCase(line.charAt(i))) {
+			// for each character in line
+			for (int i = 0; i < length; i++) {
+				// check if uppercase
+				if (Character.isUpperCase(line.charAt(i))) {
 							
-						// convert ASCII value of char to a value between 0-25 to make wraparound work
-						int rot25 = line.charAt(i) - 65;
+					// convert ASCII value of char to a value between 0-25 to make wraparound work
+					int rot25 = line.charAt(i) - 65;
 							
-						// calculate the cipher character
-						int cipher_char = (rot25 + shift) % 26;
+					// calculate the cipher character
+					int cipher_char = (rot25 + shift) % 26;
 
-						// converts to ASCII value again and place in StringBuilder
-						encodedString.append((char)(cipher_char + 65));
-					}
-					// if not uppercase just add to StringBuilder object "as is"
-					else {
-						encodedString.append((char)line.charAt(i));
-					}			
+					// converts to ASCII value again and place in StringBuilder
+					encodedString.append((char)(cipher_char + 65));
 				}
-				//add a newline
-				encodedString.append("\n");
-	        }
-	        // return string with encoded message
-	        return encodedString.toString();		
+				// if not uppercase just add to StringBuilder object "as is"
+				else {
+					encodedString.append((char)line.charAt(i));
+				}			
+			}
+			//add a newline
+			encodedString.append("\n");
+	    }
+	    inFile.close();
+	    // return string with encoded message
+	    return encodedString.toString();	
 	}
-
+	/**
+	 * Takes the name of an input file and an integer representing 
+	 * the amount of "shift" by which the contents of the file should
+	 * be decrypted and returns a decrypted string.
+	 *
+	 * @param   String inputFile
+	 * @param   int Shift
+	 * @throws: FileNotFoundException If input file can't be read
+	 */	
 	public static String caesarDecipher (String input, int shift) throws FileNotFoundException {
-			// create Scanner object
-			Scanner inFile = new Scanner(new File(input));
-		
-			// create StringBuilder object to hold deccoded lines of text
-			StringBuilder decodedString = new StringBuilder();
+		// create Scanner object
+		Scanner inFile = null;
+		try {
+			inFile = new Scanner(new File(input));
+		}
+		catch(FileNotFoundException e) {
+				System.out.println("\nSomething went wrong. That file can not be read or written to. Exiting... \n");
+		}
+		// create StringBuilder object to hold deccoded lines of text
+		StringBuilder decodedString = new StringBuilder();
 
-			// while file has nextLine
-			while (inFile.hasNextLine()) {
-				// line to be encoded
-				String line = inFile.nextLine();
+		// while file has nextLine
+		while (inFile.hasNextLine()) {
+			// line to be encoded
+			String line = inFile.nextLine();
 
-				// get length of line
-				int length = line.length(); 
+			// get length of line
+			int length = line.length(); 
 					
-				// for each character in line
-				for (int i = 0; i < length; i++) {
-					// check if uppercase
-					if (Character.isUpperCase(line.charAt(i))) {
+			// for each character in line
+			for (int i = 0; i < length; i++) {
+				// check if uppercase
+				if (Character.isUpperCase(line.charAt(i))) {
 							
-						// convert ASCII value of char to a value between 0-25 to make wraparound work
-						int rot25 = line.charAt(i) - 65;
+					// convert ASCII value of char to a value between 0-25 to make wraparound work
+					int rot25 = line.charAt(i) - 65;
 							
-						// calculate the plaintext character
-						int decipher_char = (rot25 - shift) % 26;
+					// calculate the plaintext character
+					int decipher_char = (rot25 - shift) % 26;
 
-						// converts to ASCII value again and place in StringBuilder
-						decodedString.append((char)(decipher_char + 65));
-					}
-					// if not uppercase just add to StringBuilder object "as is"
-					else {
-						decodedString.append((char)line.charAt(i));
-					}			
+					// converts to ASCII value again and place in StringBuilder
+					decodedString.append((char)(decipher_char + 65));
 				}
-				//add a newline
-				decodedString.append("\n");
-	        }
-	        // return string with decoded message
-	        return decodedString.toString();		
+				// if not uppercase just add to StringBuilder object "as is"
+				else {
+					decodedString.append((char)line.charAt(i));
+				}			
+			}
+			//add a newline
+			decodedString.append("\n");
+	    }
+	    inFile.close();
+	    // return string with decoded message
+	    return decodedString.toString();		
 	}
 }
